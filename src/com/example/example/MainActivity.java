2fb395dd11a283a2521ee06f386be3f273bb46db
yangxa <15394580570@139.com>
2015-11-02 16:44:54 +0800

    添加一个int型 i
    Signed-off-by: yangxa <15394580570@139.com>

--- src/com/example/example/MainActivity.java
+++ src/com/example/example/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	public int i;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
